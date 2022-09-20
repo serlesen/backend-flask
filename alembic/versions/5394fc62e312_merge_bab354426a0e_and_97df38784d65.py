09245a876c4aba062d135bbbeccf33981e2cfe1a
Sergio Lema <serlesen@gmail.com>
2022-09-20 22:34:36 +0200

    Merge both alembic revisions

---
+++ alembic/versions/5394fc62e312_merge_bab354426a0e_and_97df38784d65.py
@@ -0,0 +1,20 @@
+"""merge bab354426a0e and 97df38784d65
+
+Revision ID: 5394fc62e312
+Revises: bab354426a0e, 97df38784d65
+Create Date: 2022-09-20 22:32:44.483150
+
+"""
+# revision identifiers, used by Alembic.
+revision = "5394fc62e312"
+down_revision = ("bab354426a0e", "97df38784d65")
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass

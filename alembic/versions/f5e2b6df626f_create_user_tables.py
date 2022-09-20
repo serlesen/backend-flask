7aa919040f4d218d47b0ac8f60047290e7e0f0ae
Sergio Lema <serlesen@gmail.com>
2022-09-20 22:25:54 +0200

    Reset alembic scripts

--- alembic/versions/f5e2b6df626f_create_user_tables.py
+++ alembic/versions/f5e2b6df626f_create_user_tables.py
@@ -10,12 +10,7 @@ import sqlalchemy as sa
 from alembic import op
 
 # revision identifiers, used by Alembic.
-from backend.models import (
-    country_table_name,
-    message_table_name,
-    profile_table_name,
-    user_table_name,
-)
+from backend.models import country_table_name, user_table_name
 
 revision = "f5e2b6df626f"
 down_revision = "5d011ccf390c"
@@ -34,26 +29,6 @@ def upgrade() -> None:
     )
     op.create_foreign_key("fk_user_country_id", user_table_name, country_table_name, ["country_id"], ["id"])
 
-    op.create_table(
-        profile_table_name,
-        sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("birth_date", sa.DateTime),
-        sa.Column("job", sa.String(100)),
-        sa.Column("user_id", sa.Integer),
-    )
-    op.create_foreign_key("fk_profile_user_id", profile_table_name, user_table_name, ["user_id"], ["id"])
-
-    op.create_table(
-        message_table_name,
-        sa.Column("id", sa.Integer, primary_key=True),
-        sa.Column("content", sa.Text, nullable=False),
-        sa.Column("created", sa.DateTime(timezone=True)),
-        sa.Column("user_id", sa.Integer, nullable=False),
-    )
-    op.create_foreign_key("fk_message_user_id", message_table_name, user_table_name, ["user_id"], ["id"])
-
 
 def downgrade() -> None:
-    op.drop_table(message_table_name)
-    op.drop_table(profile_table_name)
     op.drop_table(user_table_name)
